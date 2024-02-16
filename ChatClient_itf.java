13e8c4c9aa2132a04c54d236b16fe3b4ddf326a1
Raphaël <113060747+Vetrarr@users.noreply.github.com>
2024-02-16 13:21:47 +0100

    Update ChatClient_itf.java

--- ChatClient_itf.java
+++ ChatClient_itf.java
@@ -4,5 +4,5 @@ import java.rmi.RemoteException;
 public interface ChatClient_itf extends Remote {
     void receiveMessage(String message, String username) throws RemoteException;
     void notify(String message) throws RemoteException;
-    String getName() throws RemoteException;
+    String getUsername() throws RemoteException;
 }
