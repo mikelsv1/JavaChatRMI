38f1cfae4008a2133dbe1fdb79f46aa080888453
Raphaël <113060747+Vetrarr@users.noreply.github.com>
2024-02-16 13:22:32 +0100

    Update Message.java

--- Message.java
+++ Message.java
@@ -1,15 +1,22 @@
+import java.io.Serializable;
 import java.util.Date;
 
-public class Message {
+public class Message implements Serializable{
 
     private String message;
     private String username;
+
     private Date timestamp;
 
     public Message(String message, String username) {
         this.message = message;
         this.username = username;
         this.timestamp = new Date();
+    }
+    public Message(Date timestamp, String username, String message) {
+        this.message = message;
+        this.username = username;
+        this.timestamp = timestamp;
     }
 
     public String getMessage() {
@@ -27,6 +34,6 @@ public class Message {
     @Override
     public String toString() {
         return timestamp + " " + username + ": " + message;
-    } 
+    }
     
 }
