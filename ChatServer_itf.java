900bbb30f2c92386dae5bcac73edb3462b3c5677
Raphaël <113060747+Vetrarr@users.noreply.github.com>
2024-02-16 13:22:10 +0100

    Update ChatServer_itf.java

--- ChatServer_itf.java
+++ ChatServer_itf.java
@@ -1,9 +1,11 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 public interface ChatServer_itf extends Remote {
     int registerClient(ChatClient_itf client, String username) throws RemoteException;
     void unregisterClient(ChatClient_itf client, String username) throws RemoteException;
     void broadcastMessage(String message, String username) throws RemoteException;
-    void getMessageHistory(ChatClient_itf client) throws RemoteException;
+    void connect(String username) throws RemoteException;
+    public List<Message> getMessageHistory(int nbmessages) throws RemoteException;
 }
