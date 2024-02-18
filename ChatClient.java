2acd76d641ceb5bc5701a65ca0d70ce54c7265ae
Raphaël <113060747+Vetrarr@users.noreply.github.com>
2024-02-18 20:15:24 +0100

    Update ChatClient.java

--- ChatClient.java
+++ ChatClient.java
@@ -1,42 +1,34 @@
+import java.io.IOException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.List;
 
-public class ChatClient extends UnicastRemoteObject implements ChatClient_itf  {
-    private String username;
-    //private int id;
-    private ChatServer_itf server;
+public class ChatClient extends UnicastRemoteObject implements ChatClient_itf {
 
-    public ChatClient(String username /*, int id*/) throws RemoteException {
-        this.username = username;
-        //this.id=id;
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_RESET = "\u001B[0m";
+
+    private String name;
+    private int id;
+
+    public ChatClient(String name) throws RemoteException {
+        this.name = name;
     }
 
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username){
-        this.username = username;
+    public String getName() {
+        return name;
     }
 
-    //public int getId() {
-    //    return id;
-    //}
-
-    //public void setId(int id) {
-    //    this.id = id;
-    //}
-
-    public List<Message> getMessageHistory(int nbMessage) throws RemoteException{
-        return server.getMessageHistory(nbMessage);
+    public int getId() {
+        return id;
     }
 
-    public void sendMessage(String message) throws RemoteException {
-        server.broadcastMessage(message, username);
+    public void setId(int id) {
+        this.id = id;
     }
+
 
     @Override
     public void receiveMessage(String message, String username) {
@@ -45,10 +37,10 @@ public class ChatClient extends UnicastRemoteObject implements ChatClient_itf  {
 
     @Override
     public void notify(String message) {
-        System.out.println("Server: " + message);
+        System.out.println(ANSI_YELLOW + message + ANSI_RESET);
     }
 
-    public static void main(String[] args) throws NotBoundException {
+    public static void main(String[] args) throws NotBoundException, IOException {
         try {
             if (args.length != 2) {
                 System.out.println("Usage: java ChatClient <host> <port>");
@@ -59,35 +51,42 @@ public class ChatClient extends UnicastRemoteObject implements ChatClient_itf  {
 
             System.out.print("Enter your username: ");
             String username = System.console().readLine();
+            
+            while (username.isEmpty()) {
+                System.out.println("Invalid username. Try again.\nEnter your username:");
+                username = System.console().readLine();
+            }
 
             ChatClient chatClient = new ChatClient(username);
             Registry registry = LocateRegistry.getRegistry(host, port);
             ChatServer_itf chatServer = (ChatServer_itf) registry.lookup("ChatServer");
-            chatClient.setUsername(username);
-            //int id = chatServer.registerClient(chatClient, username);
-            //chatClient.setId(id);
-
-            
-            // Welcome message. Exits if user types "exit". Otherwise, sends message to server. History in order to see previous messages.
-            System.out.println("Welcome to the chat, " + username + "!");
-            System.out.println("Type 'exit' to leave the chat.");
-            System.out.println("Type 'history' to see previous messages.\n");
+            int id = chatServer.registerClient(chatClient);
+            chatClient.setId(id);
             
             while (true) {
                 String message = System.console().readLine();
-                if (message.equalsIgnoreCase("exit")) {
-                    System.out.println("Goodbye!");
-                    chatServer.unregisterClient(chatClient, username);
+                if (message.equalsIgnoreCase("/exit")) {
+                    chatServer.unregisterClient(chatClient);
                     System.exit(0);
-                } else if (message.equalsIgnoreCase("history")) {
-                    List<Message> history = chatClient.getMessageHistory(25);
-                    for (Message m : history) {
-                        System.out.println(""+m.getUsername()+" : "+m.getMessage());
+                } else if (message.startsWith("/history")) {
+                    String[] parts = message.split(" ");
+                    int numberOfMessages = 0;
+                    if (parts.length > 1) {
+                        try {
+                            numberOfMessages = Integer.parseInt(parts[1]);
+                        } catch (NumberFormatException e) {
+                            System.out.println("Invalid number of messages.");
+                            continue;
+                        }
                     }
-
+                    else {
+                        numberOfMessages = Integer.MAX_VALUE;
+                    }
+                    chatServer.getMessageHistory(chatClient, numberOfMessages);
+                
                 } else {
                     try {
-                        chatServer.broadcastMessage(message, username);
+                        chatServer.broadcastMessage(message, chatClient.getId());
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
